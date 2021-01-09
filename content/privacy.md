2030fe95ab7f32266dd8344ad8b306cd53a69f85
devidwolf <68775926+devidwolf@users.noreply.github.com>
2021-01-09 20:13:32 +0100

    custom theme

---
+++ content/privacy.md
@@ -0,0 +1,19 @@
+title: privacy
+description: Read about your data on this website.
+
+<!-- https://www.udmedia.de/news/542/dsgvo-checkliste-zur-datenschutzverordnung -->
+
+### Google [Search Console][g-search-console]
+- [privacy][g-privacy]
+- [terms][g-terms]
+
+### Server Logfiles
+[hoster][ud-media-logs]
+
+<!-- ### Cookies
+one cookie to save your okay to this -->
+
+[g-search-console]: https://search.google.com/search-console
+[g-privacy]: https://policies.google.com/privacy
+[g-terms]: https://policies.google.com/terms
+[ud-media-logs]: https://www.udmedia.de/service/datenschutz/#datenschutz-1:~:text=Browsertyp%2F%2Dversion,Nutzers%20%C3%BCber%20unsere%20Website%20aufgerufen%20wird
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
+PAGE_PATHS = ['']
+# STATIC_EXCLUDES = ['sass']
+
+PAGE_SAVE_AS = '{slug}/index.html'
+PAGE_URL = '{slug}/'
+
+DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_PATH = './docs'
+
 AUTHOR = 'David Wolf'
 SITENAME = 'David Wolf'
-SITEURL = ''
+# SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
 
@@ -19,16 +28,21 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('GitHub', 'https://github.com/devidwolf'),
+         ('PayPal.Me', 'https://paypal.me/devidwolf'),
+         ('Buy me a coffee', 'https://buymeacoffee.com/dwolf'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
+
+THEME = './theme'
+
+PLUGINS = [
+    'plugins.assets',
+]
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
