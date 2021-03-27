3914003e2c6d4f27f75fe7245209bd502ec4d4d7
devidwolf <68775926+devidwolf@users.noreply.github.com>
2021-03-27 12:36:21 +0100

    fix siteurl

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,8 +6,8 @@
 AUTHOR = 'David Wolf'
 SITENAME = 'David Wolf'
 # SITENAME = 'Darksome'
-SITEURL = 'http://localhost:8000'
-# SITEURL = 'https://davidwolf.dev'
+# SITEURL = 'http://localhost:8000'
+SITEURL = 'https://davidwolf.dev'
 
 # PAGE_PATHS = ['pages']
 STATIC_PATHS = ['CNAME', 'media'] # CNAME for GitHub Pages custom apex domain
