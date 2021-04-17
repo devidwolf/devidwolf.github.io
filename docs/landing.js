cd32a846037c96ace4039dcbc02324394b99a77a
devidwolf <68775926+devidwolf@users.noreply.github.com>
2021-04-17 17:00:44 +0200

    add videos

--- docs/landing.js
+++ docs/landing.js
@@ -7,6 +7,18 @@ $(function() {
     './media/videos/landing-21054100.webm',
     './media/videos/landing-17392000-1.webm',
     './media/videos/landing-00331700.webm',
+    './media/videos/00072300.webm',
+    './media/videos/20424700.webm',
+    './media/videos/02084900.webm',
+    './media/videos/21091400.webm',
+    './media/videos/02560300.webm',
+    './media/videos/22115200.webm',
+    './media/videos/05484600.webm',
+    './media/videos/23005500.webm',
+    './media/videos/05541400.webm',
+    './media/videos/23161000.webm',
+    './media/videos/19343100.webm',
+    './media/videos/23591700.webm',
   ];
   // if not set already set random starting video index
   // console.log(utils.getCookie('sessionVid'));
